faa395d5214b2dec40cbc1ce9f511462bb8bdee7
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-12-04 23:30:12 +0000

    Done.

--- spec/keys_of_hash_spec.rb
+++ spec/keys_of_hash_spec.rb
@@ -1,6 +1,6 @@
 describe 'keys_of' do
 
-  let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+  let(:animals) {   }
     
   it "returns an array" do
     expect(animals.keys_of('Panama').class).to eq(Array)
