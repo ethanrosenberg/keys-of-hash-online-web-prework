cbf0463027176072a7fafde17cb4592ba5ed77cc
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-12-04 09:00:48 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
+
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+      arr = []
+      arguments.each do |arg|
+         self.each do |key, value|
+           if value == arg
+            binding.pry
+            arr << key
+          end
+         end 
+      end
+      #binding.pry
+     arr
   end
 end
+
+
+
